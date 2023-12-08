cdf078eae9ccb01b01d1550372574ca4bc913bef
akorzunin <akorzunin123@gmail.com>
2023-12-08 21:31:26 +0300

    feat: added collisions to destroy figures

--- scenes/figures/icosahedron/MeshIcosahedron.gd
+++ scenes/figures/icosahedron/MeshIcosahedron.gd
@@ -4,10 +4,12 @@ extends MeshInstance3D
 @export var SCALE_FACTOR := 1.009
 @export var scaling_enabled = true
 @onready var signals: Signals = $"/root/Main/Signals"
+@onready var area: Area3D = $"/root/Main/CollisionContainer"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
     signals.new_game_mode.connect(_game_mode_changed)
+    area.area_entered.connect(_on_area_exit)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -20,3 +22,6 @@ func _game_mode_changed(game_mode: GameStateManager.GameMode) -> void:
         scaling_enabled = false
     else:
         scaling_enabled = true
+
+func _on_area_exit(n: Area3D):
+    queue_free()
