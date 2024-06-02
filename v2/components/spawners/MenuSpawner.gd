6d34511512873006ba6d2ff07a3c9d037d520b88
akorzunin <akorzunin123@gmail.com>
2024-06-02 22:47:51 +0300

    feat: init menu scene

--- v2/components/spawners/MenuSpawner.gd
+++ v2/components/spawners/MenuSpawner.gd
@@ -1,9 +1,21 @@
-extends Node
+extends Node3D
 class_name MenuSpawner
 
+@onready var settings: Settings = %Settings
+const IcosahedronScene = preload('res://v2/models/icosahedron/Icosahedron.tscn')
+@onready var anchor: Marker3D = %Anchor
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+    var new_figure = IcosahedronScene.instantiate() \
+                .init(
+                    settings,
+                    #{ type = 0 },
+                    {type = 1},
+                    {quat = Quaternion(0, 0.707, 0, 0.707)}
+                )
+    if (anchor.get_child_count() <= 0):
+        anchor.add_child(new_figure)
     pass # Replace with function body.
 
 
