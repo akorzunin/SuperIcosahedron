0818fe5c468e0230de3b4f6810ce68e1ec4ddc02
akorzunin <akorzunin123@gmail.com>
2024-06-14 00:04:23 +0300

    feat: added deploy and build scripts

--- scripts/get_version.sh
+++ scripts/get_version.sh
@@ -1,3 +1,8 @@
-#!/bin/sh
+#!/bin/bash
+
+if [[ "$1" == "--short" ]]; then
+    echo $(git rev-parse --short --verify HEAD)
+    exit 0
+fi
 
 echo Version: $(git rev-parse --short --verify HEAD) at $(git branch --show-current )
