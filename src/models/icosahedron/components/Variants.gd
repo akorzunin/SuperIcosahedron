f04b7617e9c97be642a0930e469fa9ff762ff447
akorzunin <akorzunin123@gmail.com>
2024-07-02 16:50:32 +0300

    temp: new faces notation for icosahedron

--- src/models/icosahedron/components/Variants.gd
+++ src/models/icosahedron/components/Variants.gd
@@ -3,6 +3,79 @@ class_name IcosahedronVarints
 
 # magical number that represent distance between cutplane and origin
 const dst := 0.794
+const l := 0.577
+
+## 20 faces 12 vert 30 egdes
+# quad -> rect
+const figure_variants_v2 = {
+    '111' = {
+        '111' = Vector4( l, l, l, dst),
+        '110' = Vector4( l, l, l, dst),
+        '011' = Vector4( l, l, l, dst),
+        '101' = Vector4( l, l, l, dst),
+    },
+    '011' = {
+        '111' = Vector4( l, l, l, dst),
+        '110' = Vector4( l, l, l, dst),
+        '011' = Vector4( l, l, l, dst),
+        '101' = Vector4( l, l, l, dst),
+    },
+    '010' = {
+        '111' = Vector4( l, l, l, dst),
+        '110' = Vector4( l, l, l, dst),
+        '011' = Vector4( l, l, l, dst),
+        '101' = Vector4( l, l, l, dst),
+    },
+    '110' = {
+        '111' = Vector4( l, l, l, dst),
+        '110' = Vector4( l, l, l, dst),
+        '011' = Vector4( l, l, l, dst),
+        '101' = Vector4( l, l, l, dst),
+    },
+    '101' = {
+        '111' = Vector4( l, l, l, dst),
+        '110' = Vector4( l, l, l, dst),
+        '011' = Vector4( l, l, l, dst),
+        '101' = Vector4( l, l, l, dst),
+    },
+    '001' = {
+        '111' = Vector4( l, l, l, dst),
+        '110' = Vector4( l, l, l, dst),
+        '011' = Vector4( l, l, l, dst),
+        '101' = Vector4( l, l, l, dst),
+    },
+    '000' = {
+        '111' = Vector4( l, l, l, dst),
+        '110' = Vector4( l, l, l, dst),
+        '011' = Vector4( l, l, l, dst),
+        '101' = Vector4( l, l, l, dst),
+    },
+    '100' = {
+        '111' = Vector4( l, l, l, dst),
+        '110' = Vector4( l, l, l, dst),
+        '011' = Vector4( l, l, l, dst),
+        '101' = Vector4( l, l, l, dst),
+    },
+}
+# 3 golden rectangles
+# rect x
+# rect y
+# rect z
+# to define eash face we ned to know what cartesian quadrant it belongs
+# and what rectangle it touches (can be 2 or all)
+# RX have normal (1,0,0) and so on
+enum GoldenRect {RX, RY, RZ}
+# so each rect can be represented as vector
+# and we can use them in conmbinations also
+
+# RX + RY = (1,1,0)
+
+
+static func get_face( rects: Array[GoldenRect], quad: Vector3i):
+    if len(rects) == 3:
+        return figure_variants_v2[quad]['111']
+    pass
+
 
 const figure_variants = {
     8: {
