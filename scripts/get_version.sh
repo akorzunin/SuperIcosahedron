1f420ff7f9f496f7fd21fb4cded2afa755697d54
akorzunin <akorzunin123@gmail.com>
2024-07-02 13:49:48 +0300

    feat: add tag to app name

--- scripts/get_version.sh
+++ scripts/get_version.sh
@@ -5,4 +5,9 @@ if [[ "$1" == "--short" ]]; then
     exit 0
 fi
 
-echo Version: $(git rev-parse --short --verify HEAD) at $(git branch --show-current )
+if [[ "$1" == "--tag" ]]; then
+    echo $(git describe --tags $(git rev-list --tags --max-count=1))
+    exit 0
+fi
+
+echo Version: $(git rev-parse --short --verify HEAD) Latest tag: $(git describe --tags $(git rev-list --tags --max-count=1)) at $(git branch --show-current )
