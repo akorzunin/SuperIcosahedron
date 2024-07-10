b584c22cebfb42a4ae9aaa84f40cf29fc137291e
akorzunin <akorzunin123@gmail.com>
2024-07-10 15:09:54 +0300

    fix: refactor cutplane scene

--- src/models/icosahedron/test/cutplanes.gd
+++ src/models/icosahedron/test/cutplanes.gd
@@ -2,6 +2,10 @@
 extends Node3D
 class_name Cutplanes
 
+const phi := 0.398
+const w_2 := .849
+const w_3 := 1.369
+
 @onready var mesh_icosahedron: MeshIcosahedron = $MeshIcosahedron
 
 @export var enable_shader := true:
@@ -13,46 +17,36 @@ class_name Cutplanes
 @export var cutplane := Vector4(-0.577, -0.577, -0.577, IcosahedronVarints.dst):
     set(val):
         cutplane = val
-        var m = mesh_icosahedron.get_active_material(0) as ShaderMaterial
-        m.set_shader_parameter("cutplane", cutplane)
-        #notify_property_list_changed()
+        set_shader_cutplane(val)
 
 enum IcoAngle {ZERO_0, ONE_1, NEG_ONE_1, PHI, NEG_PHI, W_1, W_2, W_3}
 @export var x := IcoAngle.ONE_1:
     set(val):
         x = val
         if not set_silent:
-            var m = mesh_icosahedron.get_active_material(0) as ShaderMaterial
             cutplane.x = angle_to_float(val)
-            m.set_shader_parameter("cutplane", cutplane)
-            notify_property_list_changed()
+            upd_cutplane()
 
 @export var y := IcoAngle.ONE_1:
     set(val):
         y = val
         if not set_silent:
-            var m = mesh_icosahedron.get_active_material(0) as ShaderMaterial
             cutplane.y = angle_to_float(val)
-            m.set_shader_parameter("cutplane", cutplane)
-            notify_property_list_changed()
+            upd_cutplane()
 
 @export var z := IcoAngle.ONE_1:
     set(val):
         z = val
         if not set_silent:
-            var m = mesh_icosahedron.get_active_material(0) as ShaderMaterial
             cutplane.z = angle_to_float(val)
-            m.set_shader_parameter("cutplane", cutplane)
-            notify_property_list_changed()
+            upd_cutplane()
 
 @export var w := IcoAngle.W_3:
     set(val):
         w = val
         if not set_silent:
-            var m = mesh_icosahedron.get_active_material(0) as ShaderMaterial
             cutplane.w = angle_to_float(val)
-            m.set_shader_parameter("cutplane", cutplane)
-            notify_property_list_changed()
+            upd_cutplane()
 
 var set_silent := false
 @export var trans := TransformType.NONE:
@@ -68,10 +62,21 @@ var set_silent := false
         set_silent = false
         notify_property_list_changed()
 
+func set_shader_cutplane(_cutplane: Vector4):
+    if not mesh_icosahedron:
+        return
+    var m = mesh_icosahedron.get_active_material(0) as ShaderMaterial
+    m.set_shader_parameter("cutplane", _cutplane)
+
+func upd_cutplane():
+    set_shader_cutplane(cutplane)
+    notify_property_list_changed()
+
 
 enum TransformType {NONE, NEG_X, NEG_Y, NEG_Z, SWAP_X}
 func get_next_cutplane(_cutplane, transfrom):
-    var diag_vec = _cutplane.x == _cutplane.y and _cutplane.y == _cutplane.z
+    var diag_vec = is_equal_approx(_cutplane.x, _cutplane.y) \
+        and is_equal_approx(_cutplane.y, _cutplane.z)
     match transfrom:
         TransformType.NEG_X:
             _cutplane.x = -_cutplane.x
@@ -88,15 +93,16 @@ func get_next_cutplane(_cutplane, transfrom):
                 _cutplane.y = 0.
                 _cutplane.w = w_2
             else:
-                _cutplane = Vector4(sign(max(_cutplane.x, 1.)),sign(max(cutplane.y, 1.)),sign(max(cutplane.z, 1.)), w_3)
+                _cutplane = Vector4(
+                    sign(max(_cutplane.x, 1.)),
+                    sign(max(cutplane.y, 1.)),
+                    sign(max(cutplane.z, 1.)),
+                    w_3
+                )
     return _cutplane
 
-const phi := 0.398
-const w_2 := .849
-const w_3 := 1.369
 
 func angle_to_float(ia: IcoAngle) -> float:
-    var phi = 0.398
     match ia:
         IcoAngle.ZERO_0:
             return 0.
@@ -111,31 +117,27 @@ func angle_to_float(ia: IcoAngle) -> float:
         IcoAngle.W_1:
             return IcosahedronVarints.dst
         IcoAngle.W_2:
-            return .849
+            return w_2
         IcoAngle.W_3:
-            return 1.369
-    return .5
+            return w_3
+    return 1.
 
 func float_to_angle(f: float) -> IcoAngle:
-    var phi = 0.398
-    var neg_phi = phi * -1.
-    var neg_one = -1.
-    var epsilon = 0.00001
-    if abs(f - 0.) < epsilon:
+    if is_equal_approx(f, 0.):
         return IcoAngle.ZERO_0
-    elif abs(f - 1.) < epsilon:
+    elif is_equal_approx(f, 1.):
         return IcoAngle.ONE_1
-    elif abs(f + 1.) < epsilon:
+    elif is_equal_approx(f, 1.):
         return IcoAngle.NEG_ONE_1
-    elif abs(f - phi) < epsilon:
+    elif is_equal_approx(f, phi):
         return IcoAngle.PHI
-    elif abs(f + phi) < epsilon:
+    elif is_equal_approx(f, phi):
         return IcoAngle.NEG_PHI
-    elif abs(f - IcosahedronVarints.dst) < epsilon:
+    elif is_equal_approx(f, IcosahedronVarints.dst):
         return IcoAngle.W_1
-    elif abs(f - 0.849) < epsilon:
+    elif is_equal_approx(f, w_2):
         return IcoAngle.W_2
-    elif abs(f - 1.369) < epsilon:
+    elif is_equal_approx(f, w_3):
         return IcoAngle.W_3
     return IcoAngle.ONE_1
 
