e4b706a6145a0523a0c2f2e5f8ed9a49fb13fcb2
akorzunin <akorzunin123@gmail.com>
2024-07-15 22:00:30 +0300

    fix: config selector

--- src/components/settings/Config.gd
+++ src/components/settings/Config.gd
@@ -1,23 +1,25 @@
 extends Node
 class_name Config
 
+const user_config := "user://settings.cfg"
+
 enum ConfigName {USER, LOCAL, LOOP_SCENE, MENU_SCENE}
 @export var config_name := ConfigName.USER:
     set(val):
         config_name = val
         match val:
             ConfigName.USER:
-                config = "user://settings.cfg"
+                config = user_config
             ConfigName.LOCAL:
                 config = "res://src/components/settings/default_settings.cfg"
 
-var config := "user://settings.cfg"
+var config := user_config
 
 signal set_fps_counter_state(state: bool)
 
 func _init() -> void:
-    if OS.has_feature("editor") and not config:
-        config = "res://src/components/settings/default_settings.cfg"
+    if not OS.has_feature("editor"):
+        config = user_config
     if not G.settings:
         var gs = SettingsConfig.load_gs(config)
         G.settings = gs
