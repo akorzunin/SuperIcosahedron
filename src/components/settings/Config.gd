84120677a301997e36364c484154fb7f92d906bc
akorzunin <akorzunin123@gmail.com>
2024-07-15 12:33:08 +0300

    feat: config picker

--- src/components/settings/Config.gd
+++ src/components/settings/Config.gd
@@ -1,13 +1,22 @@
 extends Node
 class_name Config
 
-# TODO enum of presets
-@export_file() var config := "user://settings.cfg"
+enum ConfigName {USER, LOCAL, LOOP_SCENE, MENU_SCENE}
+@export var config_name := ConfigName.USER:
+    set(val):
+        config_name = val
+        match val:
+            ConfigName.USER:
+                config = "user://settings.cfg"
+            ConfigName.LOCAL:
+                config = "res://src/components/settings/default_settings.cfg"
+
+var config := "user://settings.cfg"
 
 signal set_fps_counter_state(state: bool)
 
 func _init() -> void:
-    if OS.has_feature("editor"):
+    if OS.has_feature("editor") and not config:
         config = "res://src/components/settings/default_settings.cfg"
     if not G.settings:
         var gs = SettingsConfig.load_gs(config)
