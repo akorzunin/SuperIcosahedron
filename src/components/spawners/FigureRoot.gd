bfbbeff69194cf97462cf48f5bbaec430d079b97
akorzunin <akorzunin123@gmail.com>
2024-07-24 22:10:25 +0300

    feat: added visual spawn point

--- src/components/spawners/FigureRoot.gd
+++ src/components/spawners/FigureRoot.gd
@@ -6,8 +6,7 @@ class_name FigureRoot
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    if not G.settings.get("DEBUG_VISUAL", false):
-        spawn_point.hide()
+    pass
 
 func add_figure(new_figure) -> void:
     var anchor = get_node("Anchor")
