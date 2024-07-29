d4de92af69131015c5f11abf9b0febfe554a3f04
akorzunin <akorzunin123@gmail.com>
2024-07-29 21:18:52 +0300

    feat(web): header

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -3,12 +3,19 @@ import { ModeToggle } from "../shadcn/ui/mode-toggle";
 
 export const Header = () => {
   return (
-    <>
-      <Button>download</Button>
-      <Button>source code</Button>
-      <Button>steam</Button>
-      <Button>itch</Button>
-      <ModeToggle />
-    </>
+    <div className="flex justify-between p-4">
+      <h2 className="mt-10 scroll-m-20 justify-items-start pb-2 text-3xl font-semibold tracking-tight text-primary-foreground transition-colors first:mt-0">
+        SuperIcosahedron
+      </h2>
+      <div className="flex gap-3">
+        <Button asChild>
+          <a href="/download/">download</a>
+        </Button>
+        <Button>source code</Button>
+        <Button disabled>steam</Button>
+        <Button disabled>itch</Button>
+        <ModeToggle />
+      </div>
+    </div>
   );
 };
