9599f48ab00e4e75c3e565d74cda5ee67d947e8f
akorzunin <akorzunin123@gmail.com>
2024-08-08 16:52:56 +0300

    feat(web): added links to foorter

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -1,11 +1,29 @@
-import { Button } from "../shadcn/ui/button";
+import { FaTelegram } from "react-icons/fa";
+import { BiLogoGmail } from "react-icons/bi";
 
 export const Footer = () => {
   return (
     <div className="fixed inset-x-0 bottom-0 bg-secondary">
-      <div className="mx-auto flex max-w-4xl justify-between p-6">
-        <Button>2024 - akorzunin</Button>
-        <Button>contacts</Button>
+      <div className="mx-auto flex max-w-4xl items-center justify-between p-6 text-primary-foreground">
+        <div className="text-sm">
+          2024 —{" "}
+          <a
+            href="https://github.com/akorzunin"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            @akorzunin
+          </a>
+        </div>
+        <div className="flex items-center gap-3">
+          <p>Contacts: </p>
+          <a href="https://t.me/akorzunin" target="_blank">
+            <FaTelegram />
+          </a>
+          <a href="mailto:akorzunin123@gmail.com" target="_blank">
+            <BiLogoGmail />
+          </a>
+        </div>
       </div>
     </div>
   );
--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -3,8 +3,8 @@ import { MenuButtons } from "./MenuButtons";
 
 export const Header = () => {
   return (
-    <div className="flex justify-between bg-secondary p-6">
-      <h2 className="mt-10 scroll-m-20 justify-items-start pb-2 text-3xl font-semibold tracking-tight text-primary-foreground transition-colors first:mt-0">
+    <div className="flex items-center justify-between bg-secondary p-6">
+      <h2 className="scroll-m-20 justify-items-start text-3xl font-semibold tracking-tight text-primary-foreground transition-colors first:mt-0">
         SuperIcosahedron
       </h2>
       <div className="hidden gap-3 md:flex">
