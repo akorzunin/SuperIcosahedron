5b2f644df24842dada219dfe24599de3b94df420
akorzunin <akorzunin123@gmail.com>
2024-08-12 16:45:36 +0300

    feat(web): added OpenGraph tags

--- web/src/vite-env.d.ts
+++ web/src/vite-env.d.ts
@@ -2,6 +2,8 @@
 interface ImportMetaEnv {
   readonly VITE_GAME_VERSION: string;
   readonly VITE_GAME_COMMIT: string;
+  readonly VITE_HOST_URL: string;
+  readonly VITE_OG_DESCRIPTION: string;
 }
 
 interface ImportMeta {
