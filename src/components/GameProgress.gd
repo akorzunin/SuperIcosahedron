a4caa6f6347c1f2ddc903ef3ebc5b7c6b2da007b
akorzunin <akorzunin123@gmail.com>
2024-08-14 23:30:39 +0300

    feat: added theme for loop gui

--- src/components/GameProgress.gd
+++ src/components/GameProgress.gd
@@ -4,6 +4,7 @@ class_name GameProgress
 @onready var game_state_manager: GameStateManager = %GameStateManager
 @onready var loop_timer: LoopTimer = %LoopTimer
 @onready var debug_stats_container: DebugStatsContainer = %DebugStatsContainer
+@onready var gui: LoopGui = $'../Gui'
 
 var figures_passed := 0
 var time_passed := 0.
@@ -21,18 +22,24 @@ func reset():
 func _ready() -> void:
     game_state_manager.game_state_changed.connect(_on_game_state)
 
+func start():
+    gui.show_stats_panel(true)
+
 func end():
     time_passed = loop_timer.get_raw_elapsed_time()
+    gui.show_stats_panel(false)
 
 func _on_game_state(old_state: GameStateManager.GameState, new_state: GameStateManager.GameState):
     var gs := GameStateManager.GameState
     if new_state == gs.GAME_END:
         end()
-    pass
+    elif new_state == gs.GAME_ACTIVE:
+        start()
 
 func _physics_process(delta: float) -> void:
     debug_stats_container.nodes_passed.label_text = str(figures_passed)
     debug_stats_container.time_passed.label_text = loop_timer.get_elapsed_time()
+    gui.game_state_label.set_text(str(figures_passed))
 
 func get_score():
     return "score\nnodes: %s\ntime: %s" % [
--- src/components/gui/LoopGui.gd
+++ src/components/gui/LoopGui.gd
@@ -2,8 +2,8 @@ extends Node
 class_name LoopGui
 
 @onready var game_state_manager: GameStateManager = %GameStateManager
-@onready var game_state_label: Label = $LoopUi/GameStatePanel/VBoxContainer/HFlowContainer/GameStateLabel
 @onready var loop_timer: LoopTimer = %LoopTimer
+@onready var game_state_label: Label = $LoopUi/GameStatePanel/VBoxContainer/HFlowContainer/GameStateLabel
 @onready var timer_rich_text_label: Label = $LoopUi/TimerPanel/CenterContainer/TimerRichTextLabel
 @onready var debug_stats_container: DebugStatsContainer = %DebugStatsContainer
 @onready var common_controls: CommonControls = %CommonControls
@@ -11,7 +11,6 @@ class_name LoopGui
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    game_state_manager.game_state_changed.connect(_on_game_state_changed)
     common_controls.toggle_debug_stats.connect(_on_debug_stats_toggle)
     if G.settings.SHOW_DEBUG_STATS:
         debug_stats_container.show()
@@ -22,15 +21,11 @@ func _physics_process(delta: float) -> void:
     timer_rich_text_label.set_text(loop_timer.get_elapsed_time())
     pass
 
-
-func _on_game_state_changed(old_state: GameStateManager.GameState, new_state: GameStateManager.GameState):
-    game_state_label.set_text(GameStateManager.GameStateNames[new_state])
-    var gs := GameStateManager.GameState
-    if new_state == gs.GAME_END:
-        loop_ui.hide()
-    else:
-        loop_ui.show()
-
-
 func _on_debug_stats_toggle(v: bool):
     DebugStatsContainer.toggle(v, debug_stats_container)
+
+func show_stats_panel(state: bool):
+    if state:
+        loop_ui.show()
+        return
+    loop_ui.hide()
